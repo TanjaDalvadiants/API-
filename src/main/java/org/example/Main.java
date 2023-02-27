b593c599c1a027c3d2523372e6e9135c08098e52
tatiana dalvadiants <eparis@atlassian.com>
2023-02-27 16:18:59 +0400

    all added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.surname = "Иванов";
+        post.phone = "+7 (999)-999-99-99";
+        post.passport = ("4444 № 44444444");
+        post.subscription = Boolean.parseBoolean(("true"));
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 2;
+        post.birthday.year = 1992;
+
+
+    }
+}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+    FormDate birthday = new FormDate();
+
+
+}
+
